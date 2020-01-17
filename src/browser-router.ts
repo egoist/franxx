f605e5b398568081f299cf30f1b1732a076175f6
EGOIST <0x142857@gmail.com>
2020-01-17 23:04:05 +0800

    feat: add base option

--- src/browser-router.ts
+++ src/browser-router.ts
@@ -1,8 +1,15 @@
 import { createBrowserHistory } from 'history'
 import { Router } from './router'
 
-export const createBrowserRouter = () => {
-  const history = createBrowserHistory()
+export interface BrowserRouterOptions {
+  /**
+   * The base URL of the app.
+   */
+  base?: string
+}
+
+export const createBrowserRouter = (options: BrowserRouterOptions = {}) => {
+  const history = createBrowserHistory({ basename: options.base })
   const router = new Router(history)
   return router
 }
--- src/hash-router.ts
+++ src/hash-router.ts
@@ -1,8 +1,15 @@
 import { createHashHistory } from 'history'
 import { Router } from './router'
 
-export const createHashRouter = () => {
-  const history = createHashHistory()
+export interface HashRouterOptions {
+  /**
+   * The base URL of the app.
+   */
+  base?: string
+}
+
+export const createHashRouter = (options: HashRouterOptions = {}) => {
+  const history = createHashHistory({ basename: options.base })
   const router = new Router(history)
   return router
 }
